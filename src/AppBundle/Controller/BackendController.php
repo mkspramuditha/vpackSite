4fa9ce501439494691e143c5f74fe511da0ecbea
mkspramuditha <mkspramuditha@gmail.com>
2016-12-06 10:41:17 +0530

    login part ok

--- src/AppBundle/Controller/BackendController.php
+++ src/AppBundle/Controller/BackendController.php
@@ -11,8 +11,45 @@ class BackendController extends DefaultController
     /**
      * @Route("/admin/login", name="adminLogin")
      */
-    public function indexAction(Request $request)
+    public function loginAction(Request $request)
     {
-        return $this->render('backend/adminLogin.html.twig');
+
+        $auth_checker = $this->get('security.authorization_checker');
+
+
+        $token = $this->get('security.token_storage')->getToken();
+
+        $user = $token->getUser();
+
+
+
+        $isRoleAdmin = $auth_checker->isGranted('ROLE_ADMIN');
+        if($isRoleAdmin)
+        {
+            return $this->redirect(
+                $this->generateUrl("adminDashboard")
+            );
+        }
+
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error =$authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUserName();
+        return $this->render('backend/adminLogin.html.twig',array(
+            'last_username' =>$lastUsername,
+            'error'=>$error
+        ));
+
     }
+
+    /**
+     * @Route("/admin/dashboard", name="adminDashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        var_dump('dashboard');
+        exit;
+    }
+
 }
